b57243b0d3106a282f467195e2f6ccc33ebb34be
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-14 17:20:56 -0400

    feat: mostrando municipio por provincia

--- promun/province/api/queries.py
+++ promun/province/api/queries.py
@@ -1,11 +1,14 @@
 import graphene
 from .types import TownType, ProvinceType
-from ..models import Province
+from ..models import Province, Town
 
 
 class ProvinceQuery:
     all_provinces = graphene.List(ProvinceType)
+    towns_by_province = graphene.List(TownType, province=graphene.String())
 
-    @staticmethod
-    def resolve_all_provinces(*args, **kwargs):
+    def resolve_all_provinces(self, info):
         return Province.objects.all()
+
+    def resolve_towns_by_province(self, info, province):
+        return Town.objects.filter(province__slug=province)
