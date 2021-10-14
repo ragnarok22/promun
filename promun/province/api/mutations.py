78a3d32df3414e5195a9b434f98006251f438ec0
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-14 17:34:45 -0400

    feat: create town

--- promun/province/api/mutations.py
+++ promun/province/api/mutations.py
@@ -1,7 +1,7 @@
 import graphene
-from django.contrib.auth.decorators import login_required
+from graphql import GraphQLError
 
-from promun.province.models import Province
+from promun.province.models import Province, Town
 
 
 class CreateProvince(graphene.Mutation):
@@ -16,5 +16,23 @@ class CreateProvince(graphene.Mutation):
         return CreateProvince(status=True)
 
 
+class CreateTown(graphene.Mutation):
+    status = graphene.Boolean()
+
+    class Arguments:
+        name = graphene.String()
+        province_slug = graphene.String()
+
+    @staticmethod
+    def mutate(root, info, name, province_slug):
+        try:
+            province = Province.objects.get(slug=province_slug)
+            Town.objects.create(name=name, province=province)
+            return CreateTown(status=True)
+        except Province.DoesNotExist:
+            return GraphQLError('Province slug doesn\'t exist')
+
+
 class ProvinceMutation:
     add_province = CreateProvince.Field()
+    add_town = CreateTown.Field()
